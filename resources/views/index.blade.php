5269237584a5c260c1bd39c7285b1b89d5c4741b
ph2150097 <ph2150097@transcosmos-ph.local>
2016-10-20 16:37:31 +0800

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+	<link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+<!-- <div class="wrap"> -->
+	<header>
+		<div class="wrapInner">
+			<p class="logo animated fadeInLeftShort go"><img src="{{ URL::asset('images/logo.png') }}" alt=""></p>
+		</div>
+	</header>
+	
+<!-- </div> -->
+	
+</body>
+</html>
