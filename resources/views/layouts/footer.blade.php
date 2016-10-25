5067779559fe0d51ad6c699d89bc722d092e9281
ph2150097 <ph2150097@transcosmos-ph.local>
2016-10-25 18:18:32 +0800

    Sixth Commit

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="wrapInner">
-	<p>Copyright Get Onatop &copy; 2015 <a href="{{ url('privacy') }}">Privacy Policy</a></p>
+	<p>Copyright Get Onatop &copy; 2015&nbsp;&nbsp; <a href="{{ url('privacy') }}">Privacy Policy</a></p>
 	<a href="#" class="siteLink">info@getonatop.com</a>
 </div>
